ad9642dc8ed3093f2f1adc77d19bde66e2695a7e
taiwonaf <nafiutaiwosolomon@gmail.com>
2022-06-11 23:46:35 +0100

    completed

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const hamBtn = document.getElementById("hambtn");
+const dropdownMobile = document.querySelectorAll(".dropdown");
+
+dropdownMobile.forEach(dropMobile => {
+    dropMobile.addEventListener("click", () => {
+        dropMobile.classList.toggle("activate");
+    })
+} )
 
 hamBtn.addEventListener("click", () => {
     hamBtn.parentElement.classList.toggle("active");
